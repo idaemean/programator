305e9a68468f515411bde92e1f4127b453045614
idaemean <ida.czarniecka@gmail.com>
2019-12-20 08:13:25 +0100

    Create car.js

---
+++ src/car/car.js
@@ -0,0 +1,16 @@
+class Car {
+    constructor(name, price, isDiesel) {
+        this.name = name;
+        this.price = price;
+        this.isDiesel = isDiesel;
+        this.isInsured = isInsured;
+    }
+
+    drive() {
+        return 'Silnik włączony';
+     }
+}
+
+let car = new Car('Mercedes', 35000, false, true);
+console.log(car.name); 
+console.log(car.drive()); 
