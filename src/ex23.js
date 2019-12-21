4baca6ba21b4fefefcbd687ea9c9af69d4383b13
Ida <test@o2.pl>
2019-12-21 10:17:31 +0100

    testowy commit na JS21grudzien

---
+++ src/ex23.js
@@ -0,0 +1,23 @@
+
+class Car {
+
+    constructor(make, model, yearOfProduction) {
+        this.make = make;
+        this.model = model;
+        this.yearOfProduction = yearOfProduction;
+    }
+
+    displayMakeAndModel() {
+        console.log("Make: " + this.make + ", model: " + this.model);
+        }
+
+        drive() {
+
+        console.log("Driving");
+
+        }
+    }
+
+    function createCar() {
+    let
+    }
